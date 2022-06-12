b947c4488a400271a0c958303fcdc422495acf35
liviaAugusta <liviamattoni@gmail.com>
2022-06-12 22:16:46 +0200

    progetto Vue CLI con aggiunta di componente Title

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TitleComponent/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import TitleComponent from './components/TitleComponent.Componentvue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    TitleComponent
   }
 }
 </script>
